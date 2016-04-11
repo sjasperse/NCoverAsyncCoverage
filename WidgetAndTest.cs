a91f891a3033844278635a48cf09a210242869e8
sco24559 <scott.jasperse@spectrumhealth.org>
2016-04-11 16:40:40 -0400

    All of the things

---
+++ WidgetAndTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace NCoverAsyncCoverage
+{
+    public class Widget
+    {
+        public async Task<bool> Get()
+        {
+            return await Task.FromResult(true);
+        }
+    }
+
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void GetReturnsTrue()
+        {
+            var widget = new Widget();
+
+            Assert.IsTrue(widget.Get().Result);
+        }
+    }
+}
